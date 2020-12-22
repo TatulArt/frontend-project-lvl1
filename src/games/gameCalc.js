151e6fe508a92b31508674aa2ec5dbf905732035
Tatulian Artem <artum405@gmail.com>
2020-12-22 22:36:25 +0300

    gameCalc update

--- src/games/gameCalc.js
+++ src/games/gameCalc.js
@@ -3,6 +3,40 @@ import readlineSync from 'readline-sync';
 const gameCalc = () => {
   let iterCounter = 0;
   let isAnswerRight = true;
+  let correctAnswer = '';
+  
+  // Создание функций для выполнения операций
+  const additionOperation = (firstRandomNumber, secondRandomNumber) => {
+    console.log('Question: ' + firstRandomNumber + ' + ' + secondRandomNumber);
+    correctAnswer = String(firstRandomNumber + secondRandomNumber);
+    return correctAnswer;
+  }
+
+  const subtractionOperation = (firstRandomNumber, secondRandomNumber) => {
+    console.log('Question: ' + firstRandomNumber + ' - ' + secondRandomNumber);
+    correctAnswer = String(firstRandomNumber - secondRandomNumber);
+    return correctAnswer;
+  }
+
+  const multiplicationOperation = (firstRandomNumber, secondRandomNumber) => {
+    console.log('Question: ' + firstRandomNumber + ' * ' + secondRandomNumber);
+    correctAnswer = String(firstRandomNumber * secondRandomNumber);
+    return correctAnswer;
+  }
+  //
+  
+  const makeTest = () => {
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+      iterCounter += 1;
+    } else {
+      isAnswerRight = false;
+      console.log('"' + userAnswer + '" is wrong answer ;(.Correct answer was "' + correctAnswer + '".');
+      console.log("Let's try again, " + username + '!');
+    }
+  }
 
   console.log('Welcome to the Brain Games!');
   const username = readlineSync.question('May I have your name? ');
@@ -16,55 +50,22 @@ const gameCalc = () => {
 
     // Сложение
     if (operation === 1) {
-      console.log('Question: ' + firstRandomNumber + ' + ' + secondRandomNumber);
-      const correctAnswer = String(firstRandomNumber + secondRandomNumber);
-      const userAnswer = readlineSync.question('Your answer: ');
-      // Проверка корректности ответа юзера
-      if (userAnswer === correctAnswer) {
-        console.log('Correct!');
-        iterCounter += 1;
-      } else {
-        isAnswerRight = false;
-        console.log('"' + userAnswer + '" is wrong answer ;(.Correct answer was "' + correctAnswer + '".');
-        console.log("Let's try again, " + username + '!');
-      }
-      // Проверка окончена
+      correctAnswer = additionOperation(firstRandomNumber, secondRandomNumber);
+      makeTest();
     }
     // Сложение закончилось
 
     // Вычитание
     if (operation === 2) {
-      console.log('Question: ' + firstRandomNumber + ' - ' + secondRandomNumber);
-      const correctAnswer = String(firstRandomNumber - secondRandomNumber);
-      const userAnswer = readlineSync.question('Your answer: ');
-      // Проверка корректности ответа юзера
-      if (userAnswer === correctAnswer) {
-        console.log('Correct!');
-        iterCounter += 1;
-      } else {
-        isAnswerRight = false;
-        console.log('"' + userAnswer + '" is wrong answer ;(.Correct answer was "' + correctAnswer + '".');
-        console.log("Let's try again, " + username + '!');
-      }
-      // Проверка окончена
+      correctAnswer = subtractionOperation(firstRandomNumber, secondRandomNumber);
+      makeTest();
     }
     // Вычитание закончилось
 
     // Умножение
     if (operation === 3) {
-      console.log('Question: ' + firstRandomNumber + ' * ' + secondRandomNumber);
-      const correctAnswer = String(firstRandomNumber * secondRandomNumber);
-      const userAnswer = readlineSync.question('Your answer: ');
-      // Проверка корректности ответа юзера
-      if (userAnswer === correctAnswer) {
-        console.log('Correct!');
-        iterCounter += 1;
-      } else {
-        isAnswerRight = false;
-        console.log('"' + userAnswer + '" is wrong answer ;(.Correct answer was "' + correctAnswer + '".');
-        console.log("Let's try again, " + username + '!');
-      }
-      // Проверка окончена
+      correctAnswer = multiplicationOperation(firstRandomNumber, secondRandomNumber);
+      makeTest();
     }
   // Умножение закончилось
   }
