5c4eb01d477a6d00fea6290b00d23f148ff970b7
Tatulian Artem <artum405@gmail.com>
2020-12-23 21:19:44 +0300

    gameCalc update

--- src/games/gameCalc.js
+++ src/games/gameCalc.js
@@ -6,20 +6,21 @@ const gameCalc = () => {
   const target = 'What is the result of the expression?';
 
   const makeOperation = (firstRandomNumber, secondRandomNumber, operation) => {
-    if (operation === 1) {
-      question = `Question: ${firstRandomNumber} + ${secondRandomNumber}`;
-      correctAnswer = String(firstRandomNumber + secondRandomNumber);
-    }
+    let SRN = secondRandomNumber;
+
+    question = `Question: ${firstRandomNumber} + ${secondRandomNumber}`;
 
     if (operation === 2) {
       question = `Question: ${firstRandomNumber} - ${secondRandomNumber}`;
-      correctAnswer = String(firstRandomNumber - secondRandomNumber);
+      SRN = -secondRandomNumber;
     }
 
     if (operation === 3) {
       question = `Question: ${firstRandomNumber} * ${secondRandomNumber}`;
-      correctAnswer = String(firstRandomNumber * secondRandomNumber);
+      SRN = (secondRandomNumber - 1) * firstRandomNumber;
     }
+
+    correctAnswer = String(firstRandomNumber + SRN);
   };
 
   const firstRandomNumber = Math.floor(Math.random() * (30 - 1)) + 1;
