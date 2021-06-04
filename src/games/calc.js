c5cad4f7bdc9068406e906a06fa842dc0b08965c
Tatulian Artem <artum405@gmail.com>
2021-06-04 23:02:55 +0300

    calc.js update

--- src/games/calc.js
+++ src/games/calc.js
@@ -4,19 +4,20 @@ import getRandomNumber from '../utilities.js';
 const description = 'What is the result of the expression?';
 const result = {};
 
+const operators = ['+', '-', '*'];
+
+const operations = {
+  '+': (a, b) => String(a + b),
+  '-': (a, b) => String(a - b),
+  '*': (a, b) => String(a * b),
+};
+
 const startRound = () => {
-  const operators = ['+', '-', '*'];
   const operatorIndex = getRandomNumber(1, 3);
   const operator = operators[operatorIndex];
 
   const firstNumber = getRandomNumber(1, 30);
   const secondNumber = getRandomNumber(1, 30);
-
-  const operations = {
-    '+': (a, b) => String(a + b),
-    '-': (a, b) => String(a - b),
-    '*': (a, b) => String(a * b),
-  };
 
   result.question = `Question: ${firstNumber} ${operator} ${secondNumber}`;
   result.correctAnswer = operations[operator](firstNumber, secondNumber);
