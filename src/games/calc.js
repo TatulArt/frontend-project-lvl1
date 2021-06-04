a859d351b1af315b60ce20b4a52ec3d948319e87
Tatulian Artem <artum405@gmail.com>
2021-06-04 19:00:50 +0300

    brain-calc update

--- src/games/calc.js
+++ src/games/calc.js
@@ -2,41 +2,35 @@ import run from '../index.js';
 import getRandomNumber from '../utilities.js';
 
 const description = 'What is the result of the expression?';
+const result = {};
 
-const makeOperation = () => {
-  const operation = getRandomNumber(1, 4);
+const getRound = () => {
+  const operation = getRandomNumber(1, 3);
   const firstNumber = getRandomNumber(1, 30);
-  let secondNumber = getRandomNumber(1, 30);
-
-  let correctAnswer = 1;
-  let question = '';
+  const secondNumber = getRandomNumber(1, 30);
 
   const operators = ['+', '-', '*'];
 
-const getRound = () => {
   switch (operators[operation]) {
     case '+':
-      correctAnswer = firstNumber + secondNumber;
-      question = `Question: ${firstNumber} + ${secondNumber}`;
+      result.question = `Question: ${firstNumber} + ${secondNumber}`;
+      result.correctAnswer = String(firstNumber + secondNumber);
       break;
     case '-':
-      correctAnswer = firstNumber - secondNumber;
-      question = `Question: ${firstNumber} - ${secondNumber}`;
+      result.question = `Question: ${firstNumber} - ${secondNumber}`;
+      result.correctAnswer = String(firstNumber - secondNumber);
       break;
     case '*':
-      correctAnswer = firstNumber * secondNumber;
-      question = `Question: ${firstNumber} * ${secondNumber}`;
+      result.question = `Question: ${firstNumber} * ${secondNumber}`;
+      result.correctAnswer = String(firstNumber * secondNumber);
       break;
     default:
       break;
   }
 
-  return {
-    question,
-    correctAnswer,
-  };
+  return result;
 };
 
 export default () => {
   run(getRound, description);
-}
+};
--- src/index.js
+++ src/index.js
@@ -9,13 +9,15 @@ const run = (getRound, description) => {
   for (let i = 0; i < roundsCount; i += 1) {
     const { question, correctAnswer } = getRound();
     console.log(question);
+
     const userAnswer = readlineSync.question('Your answer: ');
 
     if (userAnswer !== correctAnswer) {
-      console.log(`"${userAnswer}" is wrong answer ;(.Correct answer was "${correctAnswer}"`);
+      console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}"`);
       console.log(`Let's try again, ${userName}!`);
       return;
     }
+
     console.log('Correct!');
   }
   console.log(`Congratulations, ${userName}!`);
